55cb6641453bb2f0883f5efabb0634cb47be7b55
zhaoli <501914148@qq.com>
2019-05-28 17:57:49 +0800

    20190528

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,182 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <!--vue的魔板里面所有内容都要被一个根节点包起来-->
+    <h2>这是一个根组件</h2>
+    <h1 ref="test">{{msg}}</h1>
+    <input v-model="msg">
+    <!--ref属性获取dom-->
+    <input ref="aaa">
+    <h1>{{obj.name}}</h1>
+    <button v-on:click="getMsg()">戳我</button>
+    <br>
+    <button v-on:click="setMsg()">戳我</button>
+    <br>
+    <button v-on:click="getInputValue()">戳我</button>
+    <br>
+    <button @click="getInputValue()">戳我</button>
+    <br>
+    <!--实践对象-->
+    <button @click="eventFn($event)">事件对象</button>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="item in list">
+        {{item}}
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    <hr>
+    <br>
+    <ul><!--遍历数组中的变量-->
+      <li v-for="item in list1">
+        {{item.title}}
+      </li>
     </ul>
-    <h3>Ecosystem</h3>
+    <hr>
+    <br>
+    <ul><!--遍历数组中变量的数组-->
+      <li v-for="item in list2">
+        {{item.cate}}
+        <ol><!--带索引的遍历方式-->
+          <li v-for="(a,b) in item.list">
+          {{a}}===={{b}}
+            <ul><!--遍历对象的属性-->
+              <li v-for="c in a">
+                {{c}}
+              </li>
+              <li v-for="(d,e) in a">
+                {{d}}:{{e}}
+              </li>
+            </ul>
+          </li>
+        </ol>
+      </li>
+    </ul>
+
+    <h2 v-bind:title="title">我是一个title</h2><!--v-bind绑定属性-->
+    <img v-bind:src="src">
+    <img :src="src"><!--简写-->
+
+    <!--v-html绑定html-->
+    <span v-html="h2"></span>
+
+    <!--v-text绑定text-->
+    <span v-text="h2"></span>
+
+    <!--绑定class-->
+    <span :class="{active: isActive,blue: !isError}">111</span>
+    <hr>
+    <br>
+    <span :class="obj">111</span>
+    <hr>
+    <br>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="(item,key) in list"  :class="{blue :key==0}">
+        {{key}}----{{item}}
+      </li>
+    </ul>
+    <hr>
+    <br>
+    <ul>
+      <li v-for="(item,key) in list" :style="styleObject">
+        {{key}}----{{item}}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  /*业务逻辑定义的数据*/
+  /*
+   *
+   * 双向数据绑定mvvm
+   * 只可在表单中使用
+   * v-model
+   */
+    data(){
+      return {
+        styleObject: {
+          color: 'red',
+          fontSize: '13px'
+        },
+        isActive:true,
+        isError:false,
+        h2:"<h2>我是一个h2</h2>",
+        src:"https://ss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=1488861817,1113726833&fm=202",
+        title:"我是一个title",
+        msg:"你好vue",
+        obj:{
+          name:"张三",
+        },
+        list:["111","222","333"],
+        list1:[
+          {"title":"11111"},
+          {"title":"22222"},
+          {"title":"33333"},
+          {"title":"44444"},
+        ],
+        list2:[
+          {
+            cate:"国内新闻",
+            list:[
+              {
+                "title":"国内新闻111111"
+              },{
+                "title":"国内新闻22222"
+              },
+            ]
+          },
+          {
+            cate:"国外新闻",
+            list:[
+              {
+                "title":"国外新闻111111"
+              },{
+                "title":"国外新闻22222"
+              },
+            ]
+          },
+          {
+            cate:"西安新闻",
+            list:[
+              {
+                "title":"西安新闻111111"
+              },{
+                "title":"西安新闻22222"
+              },
+            ]
+          }
+        ]
+      }
+    },methods:{
+    /*
+    放方法的地方
+    */
+
+      getMsg(){
+        console.log(this.msg);
+    },setMsg(){
+        this.msg = "我是改变后的数据";
+    },getInputValue(){/**
+     获取input中的value
+     */
+
+
+        console.log(this.$refs.aaa.value)
+      this.$refs.test.style.color = 'red';
+    },eventFn(e){
+        //e.srcElement事件对象获取dom节点
+        e.srcElement.style.color='red';
+        console.log(e);
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.active{
+  color: red;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .blue{
+    color: blue;
+  }
 </style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id = 'ToDoList'>
+        <input v-model="todo">
+        <button @click="add()">+增加</button>
+        <br>
+        <hr>
+        <h2>进行中</h2>
+        <ul>
+            <li v-for="(item,key) in list " v-if="!item.checked">
+                <input type="checkbox" v-model="item.checked">{{item.title}}<button @click="deleteTodo(key)">删除</button>
+            </li>
+        </ul>
+        <br>
+        <hr>
+        <h2>已完成</h2>
+        <ul>
+            <li v-for="(item,key) in list "v-if="item.checked">
+                <input type="checkbox" v-model="item.checked">{{item.title}}<button @click="deleteTodo(key)">删除</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                todo: '',
+                list:[],
+            }
+            },methods:{
+                add(){
+                    const e = {
+                        title:this.todo,
+                        checked:false
+                    };
+                    this.list.push(e);
+                    this.todo = '';
+                }, deleteTodo(index){
+                    this.list.splice(index,1);
+                 },finish(e){
+                    this.list[e].checked
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
